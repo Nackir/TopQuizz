c626ffb436d58edfbffbebdca5b9cb50d60f3a92
Nackir <nackirdev@gmail.com>
2018-04-10 12:10:59 +0200

    Correction erreur TextureView au lieu de TextView

--- app/src/main/java/com/nackir/android/topquizz/MainActivity.java
+++ app/src/main/java/com/nackir/android/topquizz/MainActivity.java
@@ -2,14 +2,14 @@ package com.nackir.android.topquizz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.TextureView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // Création des variables des éléments graphiques (Texte, saisie texte et bouton)
-   private TextureView mGreetingText;
+   private TextView mGreetingText;
    private EditText mNameInput;
    private Button mPlayButton;
     @Override
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Référencer les éléments graphiques
         //Méthode permettant de trouver les éléments par leur id
-        mGreetingText = (TextureView) findViewById(R.id.activity_main_greeting_txt);
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
         mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
     }
